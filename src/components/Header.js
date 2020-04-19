34e747c2b6bbcbd5e142ba1fd82e89507a9c0bed
DC <dillonchaffey@gmail.com>
2020-04-19 07:02:43 -0300

    1

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  const template = `
+    <header>
+      <h1>Paper Story</h1>
+      <p>An example web app structure based off of 
+        <a href="https://github.com/bradtraversy/vanilla-parcel-boilerplate">Vanilla Parcel Boilerplate</a> 
+      </p> 
+    </header>
+  `;
+
+  return template;
+};
+
+export default Header;
---
+++ src/components/LeafletMap.js
@@ -0,0 +1,22 @@
+import '../../node_modules/leaflet/dist/leaflet.css';
+
+import '../../node_modules/leaflet/dist/images/marker-icon-2x.png';
+import '../../node_modules/leaflet/dist/images/marker-shadow.png';
+
+import '../../node_modules/leaflet/dist/leaflet';
+
+const LeafletMap = () => {
+  var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+  }).addTo(mymap);
+}
+
+export default LeafletMap;
